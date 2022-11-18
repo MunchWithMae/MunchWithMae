dd6b6a0c7c19ea317750c5446cb8d2bbdffb00e7
Nova-151 <118480508+MunchWithMae@users.noreply.github.com>
2022-11-17 19:47:30 -0700

    scroll to top test

--- Recipes/Recipe.js
+++ Recipes/Recipe.js
@@ -5,7 +5,41 @@ document.getElementById('buttonContainer').addEventListener('click', () => {
 let slideIndex = 1;
 var timer;
 showSlides(slideIndex);
-addIncompleted();
+
+var scrollToTopBtn = document.getElementById("scrollToTopBtn")
+
+var rootElement = document.documentElement
+
+function handleScroll() {
+  // Do something on scroll
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+  if ((rootElement.scrollTop / scrollTotal ) > 0.80 ) {
+    // Show button
+    scrollToTopBtn.classList.add("showBtn")
+  } else {
+    // Hide button
+    scrollToTopBtn.classList.remove("showBtn")
+  }
+}
+ 
+document.addEventListener("scroll", handleScroll)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Next/previous controls
 function plusSlides(n) {
